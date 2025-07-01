b3be905398cae26ec271f621960f54901de2c7d6
Leonardo Penas <n00460162@upn.pe>
2025-06-30 22:57:27 -0500

    Agregar archivos de proyecto.

---
+++ Trabajo SCI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_SCI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("╔════════════════════════╦═════════╦═══════════════╦══════════════════════════════╗                                       ");
+            Console.WriteLine("║        CUARTO 1        ║         ║               ║                              ║                                                                        ");
+            Console.WriteLine("║     ┌─────┐            ║         ║               ║                              ║                                        ");
+            Console.WriteLine("      │─────│            ║  BAÑO   ║               ║                              ║                                   ");
+            Console.WriteLine("      │     │            ║     ███ ║               ║                              ║                         ");
+            Console.WriteLine("      │     │            ║    ▄▄▄▄ ║               ║                              ║                        ");
+            Console.WriteLine("║     └─────┘            ║   ▄▄▄▄▄ ║   BAÑO 2      ║                              ║                    ");
+            Console.WriteLine("║                        ║         ║               ║                              ║                   ");
+            Console.WriteLine("║                                  ║               ║           SALA               ║                           ");
+            Console.WriteLine("║                                  ║               ║          COCINA              ║                    ");
+            Console.WriteLine("║                                  ║               ║                              ╠════════╗                       ");
+            Console.WriteLine("║                                  ║               ║                              ║        ║              ");
+            Console.WriteLine("╠═════════      ═════════╬═════════╩══════         ╝                                       ║                            ");
+            Console.WriteLine("║   ¥ ¥                                                                                    ║               ");
+            Console.WriteLine("║   ¥ ¥                                                                             BALCON ║                            ");
+            Console.WriteLine("╚═══════════════╗                                                                 ║        ║                                ");
+            Console.WriteLine("                ║                                                                 ║        ║              ");
+            Console.WriteLine("                ╚═════════════════════════════════════════════════════════════════╩════════╝                            ");     
+        }
+    }
+}
